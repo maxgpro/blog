3cdeb40d634abeba24fd6b32d1fbd72f33609b01
Max Gutin <wargix32bita@gmail.com>
2023-12-07 01:51:42 +0300

    feat: Reset Laravel framework

--- app/Modules/Admin/Panel/Routes/web.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-Route::group(['prefix' => 'panel', 'middleware' => []], function () {
-    Route::get('/', 'PanelController@index')->name('panel.index');
-});
--- app/Modules/Pub/Auth/Routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-Route::group(['prefix' => 'admin', 'middleware' => []], function () {
-    Route::get('login', 'LoginController@showLoginForm')->name('login');
-    Route::post('login', 'LoginController@login')->name('login.post');
-    Route::get('logout', 'LoginController@logout')->name('logout');
-});
